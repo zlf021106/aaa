b617ff487a8a1a2677f8da702b3f736cd4b2291e
张凌峰 <3034189236@com.cn>
2021-09-05 20:19:14 +0800

    .

---
+++ 作业/src/Fibonacci.java
@@ -0,0 +1,15 @@
+public class Fibonacci {
+    public static void main(String[] args){
+        int num1=1;
+        int num2=1;
+        int count =1;
+        System.out.print("1 1");
+        while(count<=28){
+            int temp=num1;
+            num1=num2;
+            num2+=temp;
+            System.out.print(num2+" ");
+            count+=1;
+        }
+    }
+}
