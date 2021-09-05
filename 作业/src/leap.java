91c1e1d1a9ddac4dc0237d8d0881d111d33ceb31
张凌峰 <3034189236@com.cn>
2021-09-05 18:48:32 +0800

    闰年个数计算

---
+++ 作业/src/leap.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class leap {
+    public static void main(String[] args) {
+        System.out.println("请输入一个年份:");
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        int m=0;
+        int k=n+100;
+        for(;n<=k;n++){
+            if((n%4==0&&n%100!=0)||n%400==0)
+                m++;
+        }
+        System.out.print("能过闰年个数" +m);
+    }
+}
