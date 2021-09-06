d194f52f536c43a53a0eaf15c07f14dcd13a8176
张凌峰 <3034189236@com.cn>
2021-09-06 16:40:28 +0800

    随机存数

--- 作业/src/随机存数.java
+++ 作业/src/随机存数.java
@@ -8,15 +8,15 @@ public static void main(String[] args){
     int min=arr[0];
     int sum=0;
     int average=0;
-    for(int i=0;i<arr.length;i++){
-        if(max<arr[i]){
-            max=arr[i];
+    for (int j : arr) {
+        if (max < j) {
+            max = j;
         }
-        if(min>arr[i]){
-            min=arr[i];
+        if (min > j) {
+            min = j;
         }
-        sum+=arr[i];
-        average=sum/20;
+        sum += j;
+        average = sum / 20;
     }
     System.out.println("最大值："+max);
     System.out.println("最小值："+min);
